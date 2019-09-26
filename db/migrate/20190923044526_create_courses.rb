31381c40227149edf89e1bff033b8f28d86505ac
MiaaaaaaZ <mirongzou@gmail.com>
2019-09-26 18:16:34 +0000

    lesson 22

--- db/migrate/20190923044526_create_courses.rb
+++ db/migrate/20190923044526_create_courses.rb
@@ -7,5 +7,6 @@ class CreateCourses < ActiveRecord::Migration[5.2]
       t.integer :user_id
       t.timestamps
     end
+    add_index :courses, :user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_034752) do
+ActiveRecord::Schema.define(version: 2019_09_26_084100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2019_09_25_034752) do
     t.text "description"
     t.decimal "cost"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+  end
+
+  create_table "images", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -43,6 +49,11 @@ ActiveRecord::Schema.define(version: 2019_09_25_034752) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_models_on_email", unique: true
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sections", force: :cascade do |t|
