a3e0d20cc5938ab1590805c2e346cdaabc52cdbc
MiaaaaaaZ <mirongzou@gmail.com>
2019-09-26 21:53:51 +0000

    s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,6 +2,7 @@
 
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
+  config.fog_public = false
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV["AWS_ACCESS_KEY"],
