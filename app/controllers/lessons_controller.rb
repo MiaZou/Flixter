b2e1ea74d43728aeeaee7c147891024562a6f0da
MiaaaaaaZ <mirongzou@gmail.com>
2019-10-03 21:48:57 +0000

    lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment, only: [:show]
+
   def show
 
   end
@@ -9,4 +12,11 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def require_enrollment
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You must enroll before viewing the lesson detial.'
+    end
+  end
+
 end
